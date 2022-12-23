f1d7e715085a4c5d273a20d85a3a8e702a76388a
Andrey Popov <andrej_1987@bk.ru>
2022-12-23 21:51:22 +0500

    Добавьте файлы проекта.

---
+++ Сalculator/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Сalculator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ControlTemplate curTemplate = new ControlTemplate();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+       
+        private void One_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Нажата кнопка 1");
+            
+        }
+    }
+}
