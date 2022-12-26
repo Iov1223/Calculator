8added477a2ead856ad7b32368faba7303e0ab93
Andrey Popov <andrej_1987@bk.ru>
2022-12-26 15:01:45 +0500

    Простой калькулятор

--- Сalculator/MainWindow.xaml.cs
+++ Сalculator/MainWindow.xaml.cs
@@ -25,11 +25,127 @@ namespace Сalculator
         {
             InitializeComponent();
         }
-       
+        private void InputNum(string num)
+        {
+            if (Screen.Text == "0")
+            {
+                Screen.Text = num;
+            }
+            else
+            {
+                Screen.Text += num;
+            }
+        }
         private void One_Click_1(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Нажата кнопка 1");
-            
+            InputNum("1");
+        }
+
+        private void Two_Click(object sender, RoutedEventArgs e)
+        {
+            InputNum("2");
+        }
+
+        private void Three_Click(object sender, RoutedEventArgs e)
+        {
+            InputNum("3");
+        }
+
+        private void Four_Click(object sender, RoutedEventArgs e)
+        {
+            InputNum("4");
+        }
+
+        private void Five_Click(object sender, RoutedEventArgs e)
+        {
+            InputNum("5");
+        }
+
+        private void Six_Click(object sender, RoutedEventArgs e)
+        {
+            InputNum("6");
+        }
+
+        private void Seven_Click(object sender, RoutedEventArgs e)
+        {
+            InputNum("7");
+        }
+
+        private void Eight_Click(object sender, RoutedEventArgs e)
+        {
+            InputNum("8");
+        }
+
+        private void Nine_Click(object sender, RoutedEventArgs e)
+        {
+            InputNum("9");
+        }
+
+        private void Zero_Click(object sender, RoutedEventArgs e)
+        {
+            InputNum("0");
+        }
+        private void InputSign(string sign)
+        {
+            if (Screen.Text != "0")
+            {
+                Screen.Text += sign;
+            }
+        }
+        private void Dot_Click(object sender, RoutedEventArgs e)
+        {
+            InputSign(".");
+        }
+
+        private void C_Click(object sender, RoutedEventArgs e)
+        {
+            if (Screen.Text != "0")
+            {
+                Screen.Text = "0";
+            }
+        }
+
+        private void X_Click(object sender, RoutedEventArgs e)
+        {
+            string str ="";
+            if (Screen.Text != "0" && Screen.Text.Length > 1)
+            {
+                for (int i = 0; i < Screen.Text.Length - 1; i++)
+                {
+                    str += Screen.Text[i];
+                }
+                Screen.Text = str;
+            }
+            else if (Screen.Text != "0" && Screen.Text.Length == 1)
+            {
+                Screen.Text = "0";
+            }
+        }
+
+        private void Multiply_Click(object sender, RoutedEventArgs e)
+        {
+            InputSign("*");
+        }
+
+        private void Divide_Click(object sender, RoutedEventArgs e)
+        {
+            InputSign("/");
+        }
+
+        private void Minus_Click(object sender, RoutedEventArgs e)
+        {
+            InputSign("-");
+        }
+
+        private void Plus_Click(object sender, RoutedEventArgs e)
+        {
+            InputSign("+");
+        }
+
+        private void Equally_Click(object sender, RoutedEventArgs e)
+        {
+            SearchExpression se = new SearchExpression();
+            Screen.Text = se.SearchResults(Screen.Text);
         }
     }
 }
---
+++ Сalculator/Сalculation.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Сalculator
+{
+    delegate string myDelegate(string myStr, string tmp);
+    internal class Сalculation
+    {
+        static public string Plus(string num1, string num2)
+        {
+            int n1 = Convert.ToInt32(num1);
+            int n2 = Convert.ToInt32(num2);
+            int res = n1 + n2;
+            string result = Convert.ToString(res);
+            return result;
+        }
+        static public string Minus(string num1, string num2)
+        {
+            int n1 = Convert.ToInt32(num1);
+            int n2 = Convert.ToInt32(num2);
+            int res = n1 - n2;
+            string result = Convert.ToString(res);
+            return result;
+        }
+        static public string Multiply(string num1, string num2)
+        {
+            int n1 = Convert.ToInt32(num1);
+            int n2 = Convert.ToInt32(num2);
+            int res = n1 * n2;
+            string result = Convert.ToString(res);
+            return result;
+        }
+        static public string Divide(string num1, string num2)
+        {
+            string result = "";
+            double n1 = Convert.ToDouble(num1);
+            double n2 = Convert.ToDouble(num2);
+            double res = 0;
+            if (n2 == 0)
+            {
+                result = "Дуление на ноль невозможно";
+            }
+            else
+            {
+                res = n1 / n2;
+                result = Convert.ToString(res);
+            }
+            return result;
+        }
+    }
+    internal class SearchExpression
+    {
+        static private Regex regex1 = new Regex(@"(\d+)", RegexOptions.IgnoreCase);
+        static private Regex regex2 = new Regex(@"[+/*-]", RegexOptions.IgnoreCase);
+        static private string a = "", result = "", b = "";
+        public string SearchResults(string Expression)
+        {
+            myDelegate calculator;
+            MatchCollection matches1 = regex1.Matches(Expression);
+
+            a = Convert.ToString(matches1[0]);
+            b = Convert.ToString(matches1[1]);
+            MatchCollection matches2 = regex2.Matches(Expression);
+            if (Convert.ToString(matches2[0]) == "*")
+            {
+                calculator = Сalculation.Multiply;
+                result = calculator(a, b);
+            }
+            else if (Convert.ToString(matches2[0]) == "/")
+            {
+                calculator = Сalculation.Divide;
+                result = calculator(a, b);
+            }
+            else if (Convert.ToString(matches2[0]) == "+")
+            {
+                calculator = Сalculation.Plus;
+                result = calculator(a, b);
+            }
+            else if (Convert.ToString(matches2[0]) == "-")
+            {
+                calculator = Сalculation.Minus;
+                result = calculator(a, b);
+            }
+            return result;
+        }
+    }
+
+}
